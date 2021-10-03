128fd02d61d2b8e48023af4756b136aea7870bf2
prplblck <kariurek@gmail.com>
2021-10-03 15:39:39 +0200

    mobile navbar

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function myFunction() {
     const element = document.body;
     element.classList.toggle("dark");
     }
+
+    function hamburger() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      } 
